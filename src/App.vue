9b9d0a1248e285a901a4343e56c93d6d2933dd74
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2021-04-09 22:10:21 +0200

    Added new component - SearchInput

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="app">
-    <div id="nav">
-      <router-link to="/">spacer</router-link>
-      <router-link to="/about">about</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss" scoped/>
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+</style>
